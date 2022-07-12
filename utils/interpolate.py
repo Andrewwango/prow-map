fb2fe4052770a9d4243c22e37512298760d3b735
Andrew Wang <andrew.wang27@gmail.com>
2022-07-12 18:37:21 +0100

    dev

--- utils/interpolate.py
+++ utils/interpolate.py
@@ -1,18 +1,49 @@
 import pandas as pd
+import numpy as np
 import osmnx as ox
-from tqdm import tqdm
+from tqdm.auto import tqdm
 from shapely.geometry import LineString
 from . import utils
+from haversine import haversine, haversine_vector
 
-def geo_interpolate_df(df, lat_colname="latitude", lon_colname="longitude", trackno_colname="trackid", dist_m=20): #roughly 20m
-    ls = LineString(df[[lat_colname, lon_colname]].to_numpy())
-    interpolated = ox.utils_geo.interpolate_points(ls, dist=utils.metres_to_dist(dist_m))
-    ret_df = pd.DataFrame(interpolated, columns=[lat_colname, lon_colname])
+
+def split_dirty_track(df, dist_func="euclidean", thresh=utils.THRESH_INTERPOLATION_JUMP_DIST):
+    a = df.iloc[:-1, 0:2].to_numpy()
+    b = df.iloc[1:, 0:2].to_numpy()   
+    if dist_func=="euclidean":
+        dists = ((a-b)**2).sum(axis=1) * utils.EARTH_CONST_SQUARED
+        thresh *= thresh
+    elif dist_func=="haversine":
+        dists = haversine_vector(a, b, "m")
+    
+    split_indices = np.where(dists > thresh)[0]
+    return np.split(df, split_indices + 1)
+    
+
+def geo_interpolate_df(df, lat_colname="latitude", lon_colname="longitude", trackno_colname="trackid", dist_m=20, track_points_thresh=utils.THRESH_SPURIOUS_GPS_POINT_COUNT):   
+    segments = split_dirty_track(df) #split df into defined sections based on interpoint distance. This alleviates both the truncated edge and dirty tracks problem.
+    out_dfs = []
+    debug=[]
+    for i,segment in enumerate(segments):
+        if len(segment) <= track_points_thresh:
+            debug.append(len(segment))
+            continue
+        else:
+            ls = LineString(segment[[lat_colname, lon_colname]].to_numpy())
+            interpolated = ox.utils_geo.interpolate_points(ls, dist=utils.metres_to_dist(dist_m))
+            interpolated_df = pd.DataFrame(interpolated, columns=[lat_colname, lon_colname])
+            interpolated_df["tracksegid"] = i
+            out_dfs += [interpolated_df]
+    if len(out_dfs) == 0:
+        #print(debug)
+        return None
+    ret_df = pd.concat(out_dfs, ignore_index=True)
     ret_df[trackno_colname] = df[trackno_colname].iloc[0]
     return ret_df
 
 def batch_geo_interpolate_df(raw_df, lat_colname="latitude", lon_colname="longitude", trackno_colname="trackid", dist_m=20):
+    
     interpolated_tracks_dfs = [geo_interpolate_df(y, lat_colname=lat_colname, lon_colname=lon_colname, trackno_colname=trackno_colname, dist_m=dist_m) \
                                for x, y in tqdm(raw_df.groupby(trackno_colname, as_index=False))]
-    interpolated_tracks_df = pd.concat(interpolated_tracks_dfs, ignore_index=True)
-    return interpolated_tracks_df
+    
+    return pd.concat(interpolated_tracks_dfs, ignore_index=True)
--- utils/utils.py
+++ utils/utils.py
@@ -1,21 +1,58 @@
 import pandas as pd
+import numpy as np
 
 ADDITIONAL_EDGE_DTYPES = {"row":bool, "activity":float}
+
+THRESH_EDGE_MATCH_DIST = 20 #metres from points to edges
+THRESH_EDGE_MAX_POINT_SEPARATION_PUBLIC_GPS = 30 #metres between points on edge
+THRESH_EDGE_MAX_POINT_SEPARATION_ROW_GPS = 3000 
+THRESH_INTERPOLATION_JUMP_DIST = 200 #metres between points to stop interpolation
+THRESH_SPURIOUS_GPS_POINT_COUNT = 4 #points
+INTERPOLATION_DIST_NEAREST_EDGE = 5 #metres
+INTERPOLATION_DIST_ROW_GPS = 20 #metres
+INTERPOLATION_DIST_PUBLIC_GPS = 5 #metres
+
+MAX_ACTIVITY = 20
+
+EARTH_CONST = 111194.92664455873
+EARTH_CONST_SQUARED = 12364311711.488796
 
 def in_box(lat, long, bbox):
     return (bbox[0] < long) & (long < bbox[2]) & (bbox[1] < lat) & (lat < bbox[3])
 
 def metres_to_dist(m):
-    return m / (100 * 1000)
+    return m / EARTH_CONST
 
-def threshold_on_col(df, colname="dist", thresh=20):
+def threshold_on_col(df, colname="dist", thresh=THRESH_EDGE_MATCH_DIST):
     df_thresh = df.loc[df[colname] < thresh]
+    #TODO: delete rows for which their trackno only appears less than thresh times (i.e. 4 times)
     return df_thresh.reset_index()
 
-def match_nearest_edges(edges_df, nearest_edges, filter_condition=None):
-    matched_edges = edges_df.loc[nearest_edges]
-    matched_edges['count'] = matched_edges.groupby(['u',"v","key"])['osmid'].transform('count')
-    matched_edges = matched_edges.drop_duplicates()
+def count_unique_tracks(df, trackno_colname="trackid"):
+    return len(df[trackno_colname].unique())
+
+def count_and_count_unique_tracks(df, trackno_colname="trackid"):
+    return (len(df), len(df[trackno_colname].unique()))
+
+def match_nearest_edges(edges_df, gps_df, nearest_edges_colname="ne"):
+
+    #count gps tracks
+    gps_counted = gps_df.groupby(nearest_edges_colname, as_index=True).apply(count_and_count_unique_tracks).to_frame()
+    gps_counted.columns=["temp"]
+    gps_counted["count"], gps_counted["tracks"] = zip(*gps_counted["temp"])
+    gps_counted = gps_counted.drop(columns=["temp"])
+
+    #prepare for join
+    gps_counted.index = pd.MultiIndex.from_tuples(gps_counted.index)
+    gps_counted.index.names = ['u','v','key']
+    
+    matched_edges = pd.merge(edges_df, gps_counted, on=["u","v","key"], how="inner")
+    
+    #OLD WAY using indexing: can't transfer trackid info from nearest_edges->edges_df
+    #matched_edges = edges_df.loc[nearest_edges]
+    #matched_edges['count'] = matched_edges.groupby(['u',"v","key"])['osmid'].transform('count')
+    #matched_edges = matched_edges.drop_duplicates()
+    
     return matched_edges
 
 def merge_on_edges(df1, df2, hows=["inner", "left_only"], keys=['u','v','key'], del_cols=[]):
@@ -36,3 +73,6 @@ def merge_on_edges(df1, df2, hows=["inner", "left_only"], keys=['u','v','key'], 
         outs.append(df)
     
     return outs
+
+def raw_activity_to_percentage(a):
+    return np.clip(a * 100 / MAX_ACTIVITY , 0, 100)
